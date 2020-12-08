8538f684a8456d5b180599c677bdcd60ad44e5c8
hagbemma <75255331+hagbemma@users.noreply.github.com>
2020-12-08 20:41:59 +0100

    Test

--- sources/start.java
+++ sources/start.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 
 public class start {
     public static void main(String[] args) {
+        System.out.println("Emma test");
         String line = "", splitBy = ",";
 
         try {
