a31872b599ce04976ddeb6de8892219588d0a996
hagbemma <75255331+hagbemma@users.noreply.github.com>
2020-12-08 20:45:39 +0100

    Älskar livet det funkar

--- sources/start.java
+++ sources/start.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 
 public class start {
     public static void main(String[] args) {
+        System.out.println("Test");
         String line = "", splitBy = ",";
 
         try {
