378ac05e4bf8ce15c7bc3c6faebfe9c1f4206d72
tasharos <natrosen4@gmail.com>
2020-12-08 16:14:34 +0100

    Natasha: kod som printar ut tabeller i Java

--- sources/start.java
+++ sources/start.java
@@ -1,7 +1,24 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+// en class som läser från en CSV fil och printar ut varje rad från tabellen
+// För att testa: spara "Bread basket" tabellen som CSV, se till att namn, dag etc är separerade med komma
+
 public class start {
-    public static void main(String[] args){
-        System.out.println("Hello World!");
-        System.out.println("Calle");
-        System.out.println("Natasha");
+    public static void main(String[] args) {
+        String line = "", splitBy = ",";
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("/Users/Natasha/Desktop/HT 2020/breadCSV.csv"));
+            while ((line = br.readLine()) != null) {
+                String[] products = line.split(splitBy);
+                System.out.println("Product [Transaction = " + products[0] + ", Name = " + products[1] + ", Time = "
+                        + products[2] + ", Day of week = " + products[3] + ", Weekend/Weekday = " + products[4] + "]");
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
     }
 }
